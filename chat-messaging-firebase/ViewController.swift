5021e2762401162a0c4c51aa04f382fd0e5f637c
avendisianipar <26996951+avendisianipar@users.noreply.github.com>
2025-01-22 23:05:06 +0700

    Init Project

---
+++ chat-messaging-firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-messaging-firebase
+//
+//  Created by Avendi Sianipar on 22/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
